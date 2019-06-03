57578c712f15e646224237098c53e2f6e719996a
vagrant <vagrant@homestead>
2019-06-03 06:36:53 +0000

    生成用户认证代码

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css').version();
+// Laravel Mix 给出的方案是为每一次的文件修改做哈希处理。只要文件修改，
+// 哈希值就会变，提醒客户端需要重新加载文件，很巧妙地解决了我们的问题。
+// 我们只需要对 webpack.mix.js 稍作修改，即可使用此功能：
+// 以上可看出，我们只是增加了 version() 函数的调用，其他未做修改。
