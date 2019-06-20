ae8bcbce4f3af2d791d32321ab9a96a5f11abda4
yuzhong <1046501164@qq.com>
2019-06-20 16:39:14 +0800

    显示个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
@@ -11,5 +12,16 @@ class UsersController extends Controller
     {
         return view('users.show',compact('user'));
         // Laravel 会自动解析定义在控制器方法（变量名匹配路由片段）中的 Eloquent 模型类型声明。在上面代码中，由于 show() 方法传参时声明了类型 —— Eloquent 模型 User，对应的变量名 $user 会匹配路由片段中的 {user}，这样，Laravel 会自动注入与请求 URI 中传入的 ID 对应的用户模型实例。
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(UserRequest $request,User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show',$users->id)->with('success','个人资料更新成功！');
     }
 }
