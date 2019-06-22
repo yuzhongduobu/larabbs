25e7aa6d51ee8d1b1e3344138c9e8e11fbf177ee
yuzhong <1046501164@qq.com>
2019-06-22 17:19:28 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -4,4 +4,8 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
 // 此方法会将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,4 @@ Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 */
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
