05531a8b024d537dfc898c35bac9549aa4db7cda
yuzhong <1046501164@qq.com>
2019-06-24 23:09:56 +0800

    翻译URL标示

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
     }
 }
